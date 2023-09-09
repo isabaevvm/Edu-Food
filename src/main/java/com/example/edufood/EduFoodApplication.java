1d05ff7f8839b78145592add0b8b1c1114d74297
Muhammed Isabaev <isabaevvm@gmail.com>
2023-09-09 11:31:41 +0600

    initial commit

---
+++ src/main/java/com/example/edufood/EduFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.edufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/edufood/EduFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
