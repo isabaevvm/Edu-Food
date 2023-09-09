1ec46ed7686136c1eef22cb89cf1f42533af1e62
Muhammed Isabaev <isabaevvm@gmail.com>
2023-09-09 16:47:56 +0600

    added login page

---
+++ src/main/java/com/example/edufood/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.edufood.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "auth/login";
+    }
+}
--- src/main/java/com/example/edufood/controller/RestaurantController.java
+++ src/main/java/com/example/edufood/controller/RestaurantController.java
@@ -2,12 +2,12 @@ package com.example.edufood.controller;
 
 import com.example.edufood.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantService service;
@@ -17,7 +17,7 @@ public class RestaurantController {
             Model model,
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "size", defaultValue = "5") int size
-    ){
+    ) {
         model.addAttribute("restaurants", service.getAllRestaurants(page, size));
         return "restaurants";
     }
