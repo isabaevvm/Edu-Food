d8c7fa1b435e1913f5553570880250259d14148e
Muhammed Isabaev <isabaevvm@gmail.com>
2023-09-12 00:08:24 +0600

    added controller/service/repository for orders

---
+++ src/main/java/com/example/edufood/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.example.edufood.controller;
+
+import com.example.edufood.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class OrderController {
+    private final OrderService service;
+}
