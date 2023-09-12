f670d45b034e08bc7f23364cf7bea03b10c19953
Muhammed Isabaev <isabaevvm@gmail.com>
2023-09-12 11:23:23 +0600

    refactored pages and added functions

--- src/main/java/com/example/edufood/controller/OrderController.java
+++ src/main/java/com/example/edufood/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.edufood.controller;
 import com.example.edufood.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/cart")
 public class OrderController {
     private final OrderService service;
+
+    @GetMapping
+    public String getCartPage(){
+        return "orders/cart";
+    }
 }
